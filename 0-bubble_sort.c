3fd569e93326e533c2b9ea48f9ddfc01dccd5cb1
sallebruno <9392@holbertonstudents.com>
2024-08-07 17:48:33 +0000

    task 0 finished

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,7 +1,8 @@
 #include "sort.h"
 
 /**
- * bubble_sort -  sorts an array of integers in ascending order using the Bubble sort algorithm
+ * bubble_sort -  sorts an array of integers in ascending order,
+ *					using the Bubble sort algorithm
  * @array: pointer to array
  * @size: size of array
  */
